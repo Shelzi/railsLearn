d374dce3c85cffda6bf10e57fe86ee035a5e9183
Anton (Shelzi) Nemtsou <nemant38@gmail.com>
2024-04-13 23:20:13 +0200

    add new model comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_203236) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_13_180051) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +34,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_09_203236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "articles"
 end
